d8bed8a90825f1185b79e3cb1bcb97a09398d6c8
PhABC <ph.castonguay@gmail.com>
2016-07-17 12:49:11 -0400

    Variables tracking and plotting evolution

--- graphs.py
+++ graphs.py
@@ -1,13 +1,15 @@
 import time
 import datetime
 
-import numpy             as np
-import matplotlib.pyplot as plt
-
 from activeConn.tools import *
 from activeConn       import activeConnMain as ACM
+from IPython          import display
 
-import tensorflow as tf
+import numpy                as np
+import matplotlib.pyplot    as plt
+import tensorflow           as tf
+#import matplotlib.animation as anim
+
 from tensorflow.python.ops             import rnn, rnn_cell
 from tensorflow.python.ops             import variable_scope        as vs
 from tensorflow.python.ops.constant_op import constant  as const
@@ -111,13 +113,11 @@ class actConnGraph(object):
     '''
 
 
-    # ----------------------------------------- VARIABLES ----------------------------------------- #
-
     def __init__(self, featDict ):
 
         #Default model parameters
-        defaults = {  'learnRate': 0.0001, 'nbIters':10000, 'batchSize': 50, 
-                      'dispStep'  :200, 'model': '__NGCmodel__',
+        defaults = {  'learnRate': 0.0001, 'self.nbIters':10000, 'batchSize': 50, 
+                      'self.dispStep'  :200, 'model': '__NGCmodel__',
                       'actfct':tf.tanh,'seqLen':10, 'method':1, 't2Dist':1   }      
 
         #Updating default params with featDict
@@ -152,13 +152,13 @@ class actConnGraph(object):
             self.variables = tf.trainable_variables() 		
 
             #Variables assigned to each of the variable name 
-            self.vnames    = {v.name:v for v in self.variables} 
+            self.vnames = {v.name:v for v in self.variables} 
 
             # Define loss and optimizer
             
             #Cost for connectivity in the network cell (H->H)
             #self.sparsC  = tf.add_n([ tf.nn.l2_loss( self.vnames['ng_IH_HH/MultiRNNCell/Cell0/BasicRNNCell/Linear/Matrix:0'][self.nhidNetw:,:] ) ])
-            self.sparsC  = tf.add_n([ tf.reduce_sum( tf.abs( self.vnames['ng_IH_HH/MultiRNNCell/Cell0/BasicRNNCell/Linear/Matrix:0'][self.nhidNetw:,:] )) ])
+            self.sparsC = tf.add_n([ tf.reduce_sum( tf.abs( self.vnames['ng_IH_HH/MultiRNNCell/Cell0/BasicRNNCell/Linear/Matrix:0'][self.nhidNetw:,:] )) ])
             #self.sparsC  = tf.add_n([tf.reduce_sum(tf.abs(v)) for v in self.variables]) 
             #self.sparsC  = tf.add_n([tf.nn.l2_loss(v) for v in self.variables]) 
             tf.nn.l2_loss
@@ -223,8 +223,8 @@ class actConnGraph(object):
 
         #Defining weights
         self.weights = { 
-                         'ng_H0_W' : weightInit([nhid,self.nOut], 'ng_HO_W' ), 
-                         'alpha_W' : tf.get_variable("alpha_W",[self.nInput,1]) 
+                         'ng_H0_W' : varInit([nhid,self.nOut], 'ng_HO_W' ), 
+                         'alpha_W' : varInit([self.nInput,1],  'alpha_W' ),
                         }
 
         #Defining masks
@@ -244,8 +244,11 @@ class actConnGraph(object):
                       } 
 
         #Defining biases
-        self.biases = { 'ng_H0_B' : weightInit(self.nOut, 'ng_H0_B') }  
+        self.biases = { 'ng_H0_B' : varInit(self.nOut, 'ng_H0_B') }  
 
+        #Noise distribution parameters
+        ng_Gmean = varInit([1],'ng_Gmean')
+        ng_Gstd  = varInit([1],'ng_Gstd' )
 
         #Network + Global dynamic cell (concatenated)
         ngCell = rnn_cell.BasicRNNCell(nhid, activation= self.actfct)
@@ -268,8 +271,12 @@ class actConnGraph(object):
                 #NG to output cells
                 ng_Z2 = tf.matmul(ngO, self.weights['ng_H0_W'] + self.biases['ng_H0_B'])
 
+                #Gaussian noise
+                gNoise = tf.random_normal([self.batchSize,self.nOut], mean   = ng_Gmean, 
+                                                                      stddev = ng_Gstd,  
+                                                                     dtype  = 'float32' )
                 #Prediction with calcium dynamic
-                Z2 = tf.sigmoid(tf.matmul(_Z1[i], self.weights['alpha_W']) + ng_Z2)
+                Z2 = tf.sigmoid(tf.matmul(_Z1[i], self.weights['alpha_W']) + ng_Z2 + gNoise)
 
         return Z2
 
@@ -282,8 +289,8 @@ class actConnGraph(object):
 
         #Defining the weights
         self.weights = { 
-                         'netw_dir_W' : weightInit([self.nInput]*2,            'netw_dir_W' ),
-                         'glob_HO_W'  : weightInit([self.nhidGlob, self.nOut], 'glob_HO_W'  )
+                         'netw_dir_W' : varInit([self.nInput]*2,            'netw_dir_W' ),
+                         'glob_HO_W'  : varInit([self.nhidGlob, self.nOut], 'glob_HO_W'  )
                         } 
 
         #Defining masks
@@ -294,8 +301,8 @@ class actConnGraph(object):
 
         #Defining the biases
         self.biases =  {  
-                         'netw_dir_B' : weightInit(self.nOut, 'netw_HO_B'),
-                         'glob_HO_B'  : weightInit(self.nOut, 'glob_HO_B') 
+                         'netw_dir_B' : varInit(self.nOut, 'netw_HO_B'),
+                         'glob_HO_B'  : varInit(self.nOut, 'glob_HO_B') 
                         } 
 
         #Global dynamic cell
@@ -402,15 +409,14 @@ class actConnGraph(object):
         return [Vars[i].assign(tempM[i]) for i in vidxAll]
 
 
-    def launchGraph(self, inputData, savepath = '_.ckpt', dispStep = 100, nbIters = 5000):
+    def launchGraph(self, inputData, savepath = '_.ckpt'):
         # Launch the graph
         #   Arguments ... 
         #       inputData ~ Has to be a list of 4 elements : Training_t1, training_t2
         #                                                    Testing_t1 , testing_t2.
 
-        t = time.time()
-
-        backupPath = '/tmp/backup.ckpt'
+        t          = time.time()        # Current time
+        backupPath = '/tmp/backup.ckpt' # Checkpoint backup path
 
         #Unpacking data
         T1  = inputData['T1']
@@ -427,19 +433,22 @@ class actConnGraph(object):
 
         with tf.Session(graph=self.graph) as sess:
 
-
             #Initializing the variables
             sess.run(tf.initialize_all_variables())
 
             stepTr  = 0 #Training steps
             stepBat = 0 #Testing steps
 
-            # Keep training until reach max iterations
-            while stepTr < nbIters:
+            if self.sampRate > 0:
+                samp   = 0                      #Sample
+                nbSamp = self.nbIters//self.sampRate #Number of sample
 
-                if stepTr % dispStep == 0:
+            # Keep training until reach max iterations
+            while stepTr < self.nbIters:
+
+                if stepTr % self.dispStep == 0:
                     T1_batch,T2_batch = batchCreation(T1, T2, 
-                                                      nbIters   = dispStep, 
+                                                      nbIters   = self.dispStep, 
                                                       batchSize = self.batchSize, 
                                                       seqLen    = self.seqLen)
 
@@ -455,8 +464,11 @@ class actConnGraph(object):
                 sess.run(self.masking)
 
 
-                if stepTr % dispStep == 0:
-                    testX,testY = batchCreation(Te1, Te2, nbIters=1, batchSize=self.batchSize, seqLen=self.seqLen)
+                if stepTr % self.dispStep == 0:
+                    testX,testY = batchCreation(Te1, Te2, 
+                                                nbIters   = 1, 
+                                                batchSize = self.batchSize, 
+                                                seqLen    = self.seqLen)
 
                     #Dictionnary for plotting training progress
                     feedDict_Tr = { self._T1    : T1_batch[stepBat,...], 
@@ -473,12 +485,17 @@ class actConnGraph(object):
                                                                    self._T2    : testY[0,...]  }) 
 
                     #Printing progress 
-                    print("Iter: " + str(stepTr) + "/" + str(nbIters) +   "  ~  L2 Loss: "     + "{:.6f}".format(trFit) +
+                    print("Iter: " + str(stepTr) + "/" + str(self.nbIters) +   "  ~  L2 Loss: "     + "{:.6f}".format(trFit) +
                "  ~  Sparsity loss: " + "{:.6f}".format(trSpars) + "        |  Testing fit: " + "{:.6f}".format(teFit))
 
 
                 stepTr += 1
                 stepBat += 1
+
+                #Tracking variables in v2track
+                if self.sampRate > 0 and not stepTr%self.sampRate:
+                    self._trackVar( nbSamp, samp, self.v2track )
+                    samp +=1
 
             # - - - - - - - -   - - - - -- - - - - - - - - - - -- - - 
 
@@ -486,10 +503,57 @@ class actConnGraph(object):
             self.saver.save(sess, savepath)
             self.saver.save(sess, backupPath)
 
-            print('\nTotal time:  ' + str(datetime.timedelta(seconds = time.time()-t)))
-            self.evalVars = [v.eval() for v in tf.trainable_variables()] 
+            #Saving variables final state
+            self.evalVars = {v.name: v.eval() for v in tf.trainable_variables()} 
 
-  
+            print('\nTotal time:  ' + str(datetime.timedelta(seconds = time.time()-t)))
+            
+
+
+    def _trackVar(self, nbSamp, samp, v2track):
+        ''' 
+        Will save the variables overtime at a given sampling rate.
+
+        _______________________________________________________________
+
+                                     ARGUMENTS
+        _______________________________________________________________
+
+        nbSamp  : total number of samples
+        samp    : Current sample
+        v2track : Contains the list of variables to track
+        _______________________________________________________________
+
+        '''
+
+        #Values of variables of current sample
+        vTrackVal = {v: self.vnames[v+':0'].eval() for v in v2track}
+
+        #Initializing dictionnary
+        if not hasattr(self, 'vTracked'):
+
+            self.vTracked = {}
+            for v in v2track:
+                vdim = np.hstack([nbSamp,self.vnames[v+':0'].get_shape().as_list()])
+                self.vTracked[v] = np.zeros(vdim)
+        
+        #Storing variables values at current sample
+        for v in v2track:
+            self.vTracked[v][samp,:] = vTrackVal[v]
+
+        #Storing only direct connectivity
+        if samp == nbSamp-1:
+            tt = [('RNNCell' in var and 'Matrix' in var) for var in v2track]
+            if any(tt):
+                vidx = tt.index(True) #Variable index
+
+                #If using __NGCmodel__ model
+                if self.model == '__NGCmodel__':
+                    dirC = self.vTracked[ v2track[vidx] ][: , self.nInput: -self.nhidGlob,
+                                                            :-self.nhidGlob ]
+                    self.vTracked['direct_W'] = dirC
+
+        
     def showVars(self):
         #Will plot all variables
 
@@ -523,12 +587,85 @@ class actConnGraph(object):
 
                 #Plotting one dimensional variables
                 plt.subplot(1,3,spIdx)
-                plt.plot(self.evalVars[idx])
+                plt.plot(self.evalVars[idx], 'x')
                 plt.title(v.name[:-2], fontsize = 15)
                 
             spIdx +=1
             idx   +=1
 
+
+    def vidTrack(self, v2track = None, norm = True):
+        ''' 
+            Will diplay images of tracked variables in v2track
+            over sample, giving a video. 
+
+
+        _______________________________________________________________
+
+                                    ARGUMENTS
+        _______________________________________________________________
+
+        nbSamp  : total number of samples
+        samp    : Current sample
+        v2track : Contains the list of variables to track
+        _______________________________________________________________
+
+
+        '''
+
+        if not v2track:
+            v2track = self.v2track
+
+        #Function variables 
+        nSample = self.vTracked[v2track[0]].shape[0] #Number of sample
+        nVars   = len(v2track)                  #Number of variables to display
+        nRow    = int(np.ceil(nVars/3))         #Number of subplot row
+
+        # Number of collumns
+        if   nVars  >= 3: nCol = 3 
+        elif nVars  <  3: nCol = nVars 
+
+    
+        #Using only v2track variables
+        vTracked = {v: self.vTracked[v].copy()  for v in v2track}  #Values
+        dims     = {v: vTracked[v][0,...].shape for v in vTracked} #Dimensions
+
+        #Normalizing each sample to see how things evolve
+        if norm:
+            for v in vTracked:
+                for s in range(nSample):
+                    vTracked[v][s,...] = vTracked[v][s,...]/abs(vTracked[v][s,...]).max()
+                
+        #Display 
+        fig = plt.figure(figsize = (30,15))
+        plt.hold(False)
+        for s in range(nSample):
+
+            spIdx = 1 # Subplot index
+            for v in vTracked:
+                ax = plt.subplot(nRow,nCol,spIdx)
+
+                if len(dims[v])>1 and dims[v][0]>1 and dims[v][1]>1:
+                    #Plotting 2 dimensional variables
+                    plt.imshow(vTracked[v][s,...],
+                               aspect = 'auto', clim=(-1,1),
+                               interpolation = 'None') 
+                    
+                else:
+                    #Plotting one dimensional variables
+                    plt.plot(vTracked[v][s,:], 'x', ms = 20, mew = 3)
+                    ax.set_ylim(-1,1)
+                
+                plt.title(v, fontsize = 15)
+                spIdx += 1
+
+                plt.draw()
+
+            display.clear_output(wait=True)
+            display.display(plt.gcf())
+
+        
+        display.clear_output()
 
 
 def plotfit(paramFile, nbPoints = 4000, ckpt='/tmp/backup.ckpt'):
@@ -597,6 +734,7 @@ def plotfit(paramFile, nbPoints = 4000, ckpt='/tmp/backup.ckpt'):
 
     plt.show()
 
+    return graphFit, dataDict
 
 
 #    def sensitivityA(self, data, ckpt='/tmp/backup.ckpt'):
