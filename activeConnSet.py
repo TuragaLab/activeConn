467bbd775792ac9a2998748226418aedc84c216a
PhABC <ph.castonguay@gmail.com>
2016-07-20 23:36:25 -0400

    adding classi model

--- activeConnSet.py
+++ activeConnSet.py
@@ -14,72 +14,88 @@ import tensorflow as tf
 
 
 def activeConn(paramDict, data, run = True):
-	'''
-	Bayesian RNN for Active Connectomics Learning
-	       
-		 Bayesian neural network that allows the 
-		 prediction of causal connectivity coupled with 
-		 pertubation of the system for stronger inferences. 
-	_______________________________________________________________
+    '''
+    Bayesian RNN for Active Connectomics Learning
+           
+         Bayesian neural network that allows the 
+         prediction of causal connectivity coupled with 
+         pertubation of the system for stronger inferences. 
+    _______________________________________________________________
 
-							ARGUMENTS
-	_______________________________________________________________
+                            ARGUMENTS
+    _______________________________________________________________
 
-	paramDict: Contains all the parameters passed into graph,
-		  	   which will be unpacked and added to class attributes
-	data     : Data matrix of size (nb_input_units x time)
-	run      : Will run the model or not
-	_______________________________________________________________
-
-
-	'''
-
-	# Parameters attribution ---------------------------------------------------------
-	nInput = data.shape[0] # Number of units in data
-
-	#Default model parameters
-	pDict = {
-			   'dataset':'FR_RNN.mat', '_mPath': expanduser("~") + '/.activeConn/',
-	      	   'saveName': 'ckpt.ckpt', 'learnRate': 0.0001, 'nbIters':10000,
-	           'batchSize': 50, 'dispStep':200, 'model': '__NGCmodel__',
-	           'actfct':tf.tanh,'seqLen':10, 'method':1, 't2Dist':1 , 'sampRate':0             
-	         }       
-
-	#Updatating pDict with input dictionnary
-	pDict.update(paramDict)
-
-	#Default for neural network architecture
-	if 'nInput'   not in pDict: pDict[ 'nInput'   ] = nInput
-	if 'nhidNetw' not in pDict: pDict[ 'nhidNetw' ] = nInput
-	if 'nOut'     not in pDict: pDict[ 'nOut'     ] = nInput
-
-	#Verifying if data is consistent with model
-	if nInput != pDict['nInput']:
-		raise ValueError('Number of input units in the data ({0})'.format(nInput) +
-					 ' and number of input units in the model ({0})'.format(pDict['nInput']) +
-					 ' need to be equal.')
-
-	# Path
-	savepath = pDict['_mPath'] + 'checkpoints/' + pDict['saveName'] # Checkpoint save path
+    paramDict: Contains all the parameters passed into graph,
+               which will be unpacked and added to class attributes
+    data     : Data matrix of size (nb_input_units x time)
+    run      : Will run the model or not
+    _______________________________________________________________
 
 
-	# Graph build and execution --------------------------------------------------------
+    '''
 
-	#Formatting data
-	dataDict = prepare_data(data, 
-						pDict['seqLen'], 
-						method = pDict['method'], 
-						t2Dist = pDict['t2Dist']
-						)
+    #Default model parameters
+    pDict = {
+               'dataset':'FR_RNN.mat', '_mPath': expanduser("~") + '/.activeConn/',
+               'saveName': 'ckpt.ckpt', 'learnRate': 0.0001, 'nbIters':10000,
+               'batchSize': 50, 'dispStep':200, 'model': '__NGCmodel__',
+               'actfct':tf.tanh,'seqLen':10, 'method':1, 'YDist':1 , 'sampRate':0             
+             }       
 
-	#Build graph
-	print('Building Graph    ...')
-	graph = actConnGraph(pDict)
+    #Updatating pDict with input dictionnary
+    pDict.update(paramDict)
 
-	#Launch Graph
-	if run:
-		print('Launching Session ...')
-		graph.launchGraph( dataDict, savepath = savepath )
+    #Verifying if data is consistent with model
 
-	return graph, dataDict
+    # Path
+    savepath = pDict['_mPath'] + 'checkpoints/' + pDict['saveName'] # Checkpoint save path
+
+
+    #Formatting data
+    if type(data) is dict:
+        if 'class' in pDict['model']:
+            pDict['nInput'] = 1
+            dataDict = dataPrepClassi( data,
+                                       target  = pDict['target'],
+                                       receiver= pDict['receiver'],
+                                       method  = pDict['method'],
+                                       seqLen  = pDict['seqLen'], 
+                                       aftSLen = pDict['YDist']   )
+        else:
+            dataDict = dataPrepGenerative( data['dataset'], 
+                                       seqLen = pDict['seqLen'],
+                                       method = pDict['method'], 
+                                       YDist  = pDict['YDist']   )
+
+        tempD = {key: data[key] for key in data if key not in 'dataset'}
+        dataDict.update(tempD)
+    else:
+        dataDict = dataPrepGenerative( data, 
+                                       seqLen = pDict['seqLen'],
+                                       method = pDict['method'], 
+                                       YDist  = pDict['YDist']  
+                                       )
+
+    #InputSize warming
+    if type(data) is not dict:
+        nInput = dataDict['Xtr'].shape[2]
+        if nInput != pDict['nInput']:
+            raise ValueError('Number of input units in'       +
+                            ' the data ({0})'.format(nInput)  +
+                            ' and number of input units in'   +
+                            ' the model ({0})'.format(pDict['nInput']) +
+                            ' need to be equal.')
+
+    #Build graph
+    print('Building Graph    ...')
+    graph = actConnGraph(pDict)
+
+    #Launch Graph
+    if run:
+        print('Launching Session ...')
+        Loss = graph.launchGraph( dataDict, savepath = savepath )
+    else:
+        Loss = None
+
+    return graph, dataDict, Loss
 
