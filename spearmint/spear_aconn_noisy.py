5cdf278c06a493d7ff858f2f600d8cd2837ee601
PhABC <ph.castonguay@gmail.com>
2016-08-12 13:42:16 -0400

    spearmint setup

--- spearmint/spear_aconn_noisy.py
+++ spearmint/spear_aconn_noisy.py
@@ -1,16 +1,28 @@
 #Spearmint experiment
 
 from activeConn import activeConnMain as ACM
+from   joblib   import Parallel, delayed
 import numpy as np
 
-def main(job_id, pDict):
-  floatD = {arg: np.float32(pDict[arg]) for arg in pDict if type(pDict[arg]) is float}
-  intD   = {arg: np.int32(pDict[arg]) for arg in pDict if type(pDict[arg]) is int}
+def main(job_id, argDict):
 
-  floatD.update(intD)
+	argDict = {arg: argDict[arg][0] for arg in argDict}
 
-  print("job id: {}, pDict: {}".format(job_id, floatD))
-  loss = ACM.optoV1(floatD)[2]
-  print('  Loss: {:.6} \n'.format(loss)) 
-  return loss 
+	floatD  = {arg: np.float32(argDict[arg]) for arg in argDict if type(argDict[arg]) is float}
+	intD    = {arg: np.int32(argDict[arg])   for arg in argDict if type(argDict[arg]) is int}
 
+	#argDict.update(floatD)
+	argDict.update(intD)
+
+	print("job id: {}, argDict: {}".format(job_id, argDict))
+	nLoops = 100
+
+	accAll = np.zeros(nLoops)
+	for i in range(nLoops):
+	    G,D,L = ACM.optoV1(floatD)
+	    accAll[i] = -G.AccTe
+
+	acc = np.mean(accAll)
+
+	print('  Acc: {:.6} \n'.format(acc)) 
+	return acc 
